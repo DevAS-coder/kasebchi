6467ab97625a8659e01344c16bffc175e8811f12
DevAS <165323048+DevAS-coder@users.noreply.github.com>
2025-05-26 22:08:36 +0330

    Variant in Whole Saler Adding

--- app/(dashboard)/dashboard/WholeSalerAdding.tsx
+++ app/(dashboard)/dashboard/WholeSalerAdding.tsx
@@ -13,18 +13,18 @@ function WholeSalerAdding({ auth, setAuth }: any) {
         initial: (direction: number) => ({
             x: direction > 0 ? 300 : -300,
             opacity: 0,
-            position: 'absolute',
+            position: 'absolute' as const,
         }),
         animate: {
             x: 0,
             opacity: 1,
-            position: 'relative',
+            position: 'relative' as const,
             transition: { duration: 0.4 },
         },
         exit: (direction: number) => ({
             x: direction < 0 ? 300 : -300,
             opacity: 0,
-            position: 'absolute',
+            position: 'absolute' as const,
             transition: { duration: 0.4 },
         }),
     }
