06c871f785c092db0bfe71f9dc501b68f2392a7a
DevAS <165323048+DevAS-coder@users.noreply.github.com>
2025-05-27 00:33:28 +0330

    Remove next.config.js and update Next.js version to 15.1.8 in package.json and package-lock.json, enhancing compatibility with the latest features and optimizations.

--- app/(main)/categories/[id]/page.tsx
+++ app/(main)/categories/[id]/page.tsx
@@ -1,6 +1,10 @@
 import CategoryClient from "@/components/shared/CategoryClient";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  // انجام عملیات ناهمگام در اینجا
-  return <CategoryClient id={params.id} />;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+  return <CategoryClient id={slug} />;
 }
