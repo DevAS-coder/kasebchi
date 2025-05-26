c6bd11184c5055812a8e74c0be7d6864c2c0672b
DevAS <165323048+DevAS-coder@users.noreply.github.com>
2025-05-26 23:44:20 +0330

    Refactor Page component to directly use string type for category ID parameter, simplifying the code and improving readability.

--- app/(main)/categories/[id]/page.tsx
+++ app/(main)/categories/[id]/page.tsx
@@ -1,6 +1,6 @@
 import CategoryClient from "@/components/shared/CategoryClient";
 
-export default async function Page({ params }: { params: { id: Promise<string> } }) {
-  const id = await params.id;
-  return <CategoryClient id={id} />;
+export default async function Page({ params }: { params: { id: string } }) {
+  // انجام عملیات ناهمگام در اینجا
+  return <CategoryClient id={params.id} />;
 }
